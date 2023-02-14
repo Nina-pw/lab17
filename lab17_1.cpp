7310ac0fde06cef6edd262184eca86b0a594dde3
Pimmada <nina21551@gmail.com>
2023-02-14 13:37:19 +0700

    lab17_1.cpp completed

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,40 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(a+i) = rand()%101/100.0;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+    double sum = 0.00;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            sum = sum + *(a+j+i*M);
+        }
+        *(b+i) = sum;
+        sum = 0;
+    }
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    double sum = 0.00;
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            sum = sum + *(a+i+j*M);
+        }
+        *(b+i) = sum;
+        sum = 0;
+    }
+}
